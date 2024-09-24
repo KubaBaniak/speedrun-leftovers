dbe937d4a341c2d36e0e0c1da6199e45202933b9
kuba <kuba121201@gmail.com>
2024-09-24 16:49:24 +0200

    feat: connect backend with frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,16 @@ import { RouterProvider } from 'react-router-dom';
 import theme from './providers/theme';
 import router from './providers/routes';
 import ThemeProvider from '@mui/system/ThemeProvider';
+import { QueryClientProvider } from '@tanstack/react-query';
+import networkSettings from './utils/network'
+
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={networkSettings.queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
--- frontend/src/components/forms/signUp/SignUpForm.tsx
+++ frontend/src/components/forms/signUp/SignUpForm.tsx
@@ -8,47 +8,58 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import CheckboxField from "./CheckboxField";
 import { useState } from "react";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
+import network from '../../../utils/network';
+import { useMutation } from "@tanstack/react-query";
 
-const SUCCESS_SIGNUP_MESSAGE = "You've successfully registered on our website. To complete the registration process, please check your email"
+const SUCCESS_SIGNUP_MESSAGE = "You've successfully registered on our website. To complete the registration process, please check your email";
 
 interface ISignUpFormProps {
   closeModalCallback: () => void,
-  openSnackbarCallback: (error: string | null) => void,
+  openSnackbarCallback: (message: string | null) => void,
+}
+
+const createUserRequest = async (userData: { email: string, password: string }) => {
+  const response = await network.apiClient.post('/users', userData);
+  return response.data;
 }
 
 export default function SignUpForm({ closeModalCallback, openSnackbarCallback }: ISignUpFormProps) {
-
   const [passwordShown, setPasswordShown] = useState(false);
-  const togglePasswordVisiblity = () => {
-    setPasswordShown(passwordShown ? false : true);
-  };
+  const togglePasswordVisiblity = () => setPasswordShown(!passwordShown);
+
+  const mutation = useMutation({
+    mutationFn: createUserRequest,
+    onSuccess: () => {
+      closeModalCallback();
+      openSnackbarCallback(SUCCESS_SIGNUP_MESSAGE);
+    },
+    onError: () => {
+      openSnackbarCallback("There was an error during registration. Please try again.");
+    },
+  })
 
   const {
     register,
     handleSubmit,
-    formState: { errors, isValid, isSubmitting }
+    getValues,
+    formState: { errors, isValid, isSubmitting },
   } = useForm<SignUpFormData>({
     mode: "onChange",
     resolver: zodResolver(SignUpSchema),
   });
 
-  const onSubmit: SubmitHandler<SignUpFormData> = async data => {
-    const tempRequest = new Promise<void>(resolve => {
-      setTimeout(() => {
-        console.log(data);
-        resolve();
-      }, 2000);
-    });
-
-    await tempRequest;
-    closeModalCallback();
-    openSnackbarCallback(SUCCESS_SIGNUP_MESSAGE);
+  const onSubmit: SubmitHandler<SignUpFormData> = () => {
+    const email = getValues("email");
+    const password = getValues("password");
+    const userData = { email, password };
+    mutation.mutate(userData);
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      {isSubmitting ?
-        <StyledCircularProgress /> :
+      {isSubmitting ? (
+        <StyledCircularProgress />
+      ) : (
         <StyledFormGroup>
           <InputField
             type="email"
@@ -68,10 +79,7 @@ export default function SignUpForm({ closeModalCallback, openSnackbarCallback }:
             endAdornment={
               <InputAdornment position="end">
                 <IconButton onClick={togglePasswordVisiblity}>
-                  {passwordShown ?
-                    <Visibility /> :
-                    <VisibilityOff />
-                  }
+                  {passwordShown ? <Visibility /> : <VisibilityOff />}
                 </IconButton>
               </InputAdornment>
             }
@@ -79,46 +87,48 @@ export default function SignUpForm({ closeModalCallback, openSnackbarCallback }:
           <CheckboxField
             register={register}
             error={errors.termsAndPrivacyAccepted}
+            name={"termsAndPrivacyAccepted"}
             type={""}
             label={""}
             placeholder={""}
-            name={"termsAndPrivacyAccepted"} />
+          />
 
           <SubmitContainer>
             <SubmitFormButton text="Create an account" isDisabled={!isValid} />
           </SubmitContainer>
         </StyledFormGroup>
-      }
+      )}
       <Text>
-        Already have an account ? <UnderlineLink href="log-in">Login</UnderlineLink>
+        Already have an account? <UnderlineLink href="log-in">Login</UnderlineLink>
       </Text>
     </form>
   );
 }
 
 const StyledFormGroup = styled(FormGroup)({
-  flexDirection: 'column',
-  gap: '20px',
-})
+  flexDirection: "column",
+  gap: "20px",
+});
 
 const StyledCircularProgress = styled(CircularProgress)({
-  marginBottom: '24px',
-})
+  marginBottom: "24px",
+});
 
 const Text = styled(Typography)({
-  fontSize: '14px',
-})
+  fontSize: "14px",
+});
 
 const BoldLink = styled(Link)({
-  fontWeight: '500',
-})
+  fontWeight: "500",
+});
 
-const SubmitContainer = styled('div')({
-  width: '40%',
-  marginRight: 'auto',
-  paddingBottom: '24px',
-})
+const SubmitContainer = styled("div")({
+  width: "40%",
+  marginRight: "auto",
+  paddingBottom: "24px",
+});
 
 const UnderlineLink = styled(BoldLink)({
-  textDecoration: 'underline',
-})
+  textDecoration: "underline",
+});
+
